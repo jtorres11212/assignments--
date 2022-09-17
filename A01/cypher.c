a1f3305eefe49b8c1c8e00ffd7bf31b8b83aa7f3
Joel Torres <jtorres@goldengate.cs.brynmawr.edu>
2022-09-16 21:43:21 -0400

    doine

--- A01/cypher.c
+++ A01/cypher.c
@@ -1,3 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
 int main() {
+  char wrd[27];
+  int scramble;
+  printf("GIMME WORD\n");
+  scanf("%s", wrd);
+  printf("How scuffed do you want the word?!?\n");
+  scanf("%d", &scramble);
+  int i=0;
+  while(i<strlen(wrd)){
+    int xmas=wrd[i]+scramble;
+    wrd[i]=xmas;
+    i = i+1;
+  }
+  printf("HERE YA GO %s\n", wrd);
   return 0;
 }
+//
