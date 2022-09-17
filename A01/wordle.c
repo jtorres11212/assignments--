6b1dd6f822d8c4f2108ae01e25d3c74df718ff07
Joel Torres <jtorres@goldengate.cs.brynmawr.edu>
2022-09-16 21:14:58 -0400

    Done!

--- A01/wordle.c
+++ A01/wordle.c
@@ -5,8 +5,32 @@
 
 int main() {
   srand(time(0));
-  const char* word = chooseWord();
-  printf("cheat: %s\n", word);
+  const char* wrd = chooseWord();
+  printf("cheat: %s\n", wrd);
+  char try[5];
+  int counter=0;
+  while(counter <6){
+    printf("GIMME WORD\n");
+    scanf("%s", try);
+    if(strcmp(try,wrd)==0){
+      printf("DRATS YOU WON >:(\n");
+      counter = 6;
 
-  return 0;
-}
+    }
+    else if(try!= wrd){
+      for(int i=0; i<=5; i++){
+        if(try[i]==wrd[i]){
+          printf("%c is in the right spot\n",try[i]);}
+        else if(try[i]!= wrd[0]&& try[i]==wrd[1]||wrd[2]||wrd[3]||wrd[4]){
+          printf("%c is in the wrong spot silly!!\n",try[i]);}
+        else if(try[i]!=wrd[0]&& wrd[1] && wrd[2] && wrd[3] && wrd[4]){
+          printf("%c isnt in the word!!\n", try[i]);
+        }
+        }
+      }
+    
+    counter=counter+1;
+    }
+   return 0; 
+  }
+
