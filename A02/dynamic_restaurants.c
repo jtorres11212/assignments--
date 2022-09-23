b2bbdf90678520cb3a11e69a79b8cbeb697b5b0f
Joel Torres <jtorres@goldengate.cs.brynmawr.edu>
2022-09-23 00:21:13 -0400

    ALMOST THERE

--- A02/dynamic_restaurants.c
+++ A02/dynamic_restaurants.c
@@ -1,5 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+struct dres{
+    char name[64];
+    float rating;
+    int open; 
+    int close;
+};
 
 int main() {
+  char temp;
+  int nres;
+  printf("UwU\nhow many restuaraunts?\n");
+  scanf("%d",&nres);
+  struct dres *p;
+  p = malloc(sizeof(p)*(900));
+  for(int i=1; i<=nres; i++){
+    printf("Whats the name of the restaurant?\n");
+    scanf(" %[^\n]%*c",p[i].name);
+    printf("How well liked is this restaurant?(rating)\n");
+    scanf("%g",&p[i].rating);
+    scanf("%c",&temp);
+    printf("When does the restaurant open?\n");
+    scanf("%d",&p[i].open);
+
+    printf("When does the restaurant close?\n");
+    scanf("%d",&p[i].close);
+  }
+  for(int i=0; i<=nres; i++){
+    printf("%d)  name:%s  open:%d close:%d  rating:%g\n",i,
+    p[i].name, p[i].open, p[i].close, p[i].rating); }
+  free(p);
   return 0;
 }
--- A02/slow.c
+++ A02/slow.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main() {
+  int len;
+  char words[32];
+  printf("JSNFJN Pause Length:");
+  scanf("%d",&len);
+  printf("GIMME WORDS\n");
+  scanf("%s",words);
+  char *lot=NULL;
+  lot= malloc(sizeof(char)*32);
+  int woo=0;
+  int boo=0;
+  while(boo<strlen(words)){
+  lot[woo]=words[boo];
+  boo=boo+1;
+  for(int i=1; i<=len; i++){
+    lot[woo+i]='.';
+  }
+  woo=woo+1+len;
+  }
+  printf("%s\n",lot);
+  free(lot);
   return 0;
 }
