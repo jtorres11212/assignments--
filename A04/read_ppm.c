c039d818c9cef07738f20258697b7be3478a583f
Joel Torres <jtorres@goldengate.cs.brynmawr.edu>
2022-10-10 17:39:59 -0400

    Just turning in what I have so far

--- A04/read_ppm.c
+++ A04/read_ppm.c
@@ -5,10 +5,22 @@
 
 // Choose *one* to implement
 
-struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
-  return NULL;
-}
-
-struct ppm_pixel** read_ppm_2d(const char* filename, int* w, int* h) {
+struct ppm_pixel** read_ppm_2d(const char* file, int* w, int* h) {
+  unsigned char arr[1000];//remember to change
+  FILE *fp;
+  char p6[2];
+  fp=fopen(file,"r");
+  fgets(p6,2,file);
+  printf("gimme the width and then the height\n");
+  scanf("%d\nok now the height\n%d",&w,&h);
+  if(p6!='P6'){
+    return NULL;
+    printf("You expect me to take a file like THAT?!?!");
+    exit(0);
+  }
+  else{
+    fscanf(fp);
+  }
+  fclose(fp);
   return NULL;
 }
