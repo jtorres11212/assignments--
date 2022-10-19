91688b5b921e102206b97592881a0bad79749f2f
Joel Torres <jtorres@goldengate.cs.brynmawr.edu>
2022-10-18 20:44:33 -0400

    Read ppm done!

--- A04/read_ppm.c
+++ A04/read_ppm.c
@@ -6,21 +6,32 @@
 // Choose *one* to implement
 
 struct ppm_pixel** read_ppm_2d(const char* file, int* w, int* h) {
-  unsigned char arr[1000];//remember to change
+  char arr[1000];//remember to change
   FILE *fp;
-  char p6[2];
-  fp=fopen(file,"r");
-  fgets(p6,2,file);
-  printf("gimme the width and then the height\n");
-  scanf("%d\nok now the height\n%d",&w,&h);
-  if(p6!='P6'){
-    return NULL;
+  fp=fopen(file,"rb");
+  fgets(arr,1000,fp);
+  if(arr[0]!='P'||arr[1]!='6'){
     printf("You expect me to take a file like THAT?!?!");
-    exit(0);
+    return NULL;
   }
-  else{
-    fscanf(fp);
+  //can use fread to copy data row by row into array
+  //print out thin
+  fgets(arr,1000, fp);
+  printf("\n%s",arr);
+  fgets(arr,1000, fp);
+  printf("\n%s",arr);
+  sscanf(arr,"%d %d",w,h);
+  printf("%d %d",*w,*h);
+  fgets(arr,1000, fp);
+  printf("\n%s",arr);
+  struct ppm_pixel** image =malloc(sizeof(struct ppm_pixel*)*(*h));
+  for(int i=0;i<*h;i++){
+    struct ppm_pixel*array=malloc(sizeof(struct ppm_pixel)*(*w));
+    image[i]=array;
+    fread(array,sizeof(struct ppm_pixel),*w,fp);
   }
+  
+
   fclose(fp);
-  return NULL;
+  return image;
 }
