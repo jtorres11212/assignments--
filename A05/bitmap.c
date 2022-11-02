d06c4071505b6341d0eb70dec34fc12be29b30f1
Joel Torres <jtorres@goldengate.cs.brynmawr.edu>
2022-11-01 22:52:40 -0400

    finally done!

--- A05/bitmap.c
+++ A05/bitmap.c
@@ -2,9 +2,20 @@
 
 int main() {
   unsigned long img;
+  unsigned long track;
   scanf(" %lx", &img);
   printf("Image (unsigned long): %lx\n", img);
-
-  // todo: your code here
+  for(int i=63;i>0;i--){
+    track=(img &(0x1ul <<i))>>i;
+    if(track>0){
+    printf("@");
+    }
+  
+    else{
+      printf(" ");
+  }
+    if((i%8)==0){
+      printf("\n");
+  }}
   return 0;
 }
--- A05/glitch.c
+++ A05/glitch.c
@@ -9,9 +9,24 @@ int main(int argc, char** argv) {
     printf("usage: glitch <file.ppm>\n");
     return 0;
   }
-
-  // todo: your code here
-
+  int w;
+  int h;
+  struct ppm_pixel* pixels=read_ppm(argv[1],&w,&h);
+  for(int i=0;i<h;i++){
+    for (int x=0;x<w;x++){
+      pixels[i*(w)+x].red=(pixels[i*(w)+x].red)<<(rand()%2);
+      pixels[i*(w)+x].green=(pixels[i*(w)+x].green)<<(rand()%2);
+      pixels[i*(w)+x].blue=(pixels[i*(w)+x].blue)<<(rand()%2);
+    }
+  }
+  char* file=malloc(strlen(argv[1]+10));
+  strcpy(file,argv[1]);
+  file[strlen(file)-4]="\0";
+  strcat(file,"-glitch.ppm");
+  write_ppm(file,pixels,w,h);
+  free(pixels);
+  free(file);
   return 0;
 }
+
 
