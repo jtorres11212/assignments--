61b95ff46bc2d2bf5f6f948c976663a4f42253b1
Joel Torres <jtorres@goldengate.cs.brynmawr.edu>
2022-11-01 23:11:16 -0400

    I reached mylimit

--- A06/decode.c
+++ A06/decode.c
@@ -1,13 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "read_ppm.h"
+#include <math.h>
 
+int dec(int arr[]){
+int value = 0;
+for (int i = 7; i >= 0; i--){
+    value = value + arr[i]*pow(2.0,(double)(7-i));
+  }
+printf("%c",(char)(value));
+return value;
+}
 int main(int argc, char** argv) {
   if (argc != 2) {
     printf("usage: decode <file.ppm>\n");
     return 0;
   }
-
+  int w, h;
+  int idx = 0;
+  int b[1000];
+  struct ppm_pixel* pixels = read_ppm(argv[1],&w,&h);
+  for(int i = 0; i < h; i++){
+    for(int x = 0; x < w; x++){
+      b[idx] = (pixels[i*(w)+x].red & 0b1);
+      idx = idx +1;
+      b[idx] = (pixels[i*(w)+x].green & 0b1);
+      idx = idx +1;
+      b[idx] = (pixels[i*(w)+x].blue & 0b1);
+      idx = idx +1;
+    }
+  }
+  
+  for(int x = 0; x < idx; x++){
+      printf("%d",b[x]);
+  }
+  int dc[8];
+  printf("\n");
+  for (int x =0;x<idx;x+= 8){
+    dc[0]=b[x];
+    dc[1]=b[x+1];
+    dc[2]=b[x+2];
+    dc[3]=b[x+3];
+    dc[4]=b[x+4];
+    dc[5]=b[x+5];
+    dc[6]=b[x+6];
+    dc[7]=b[x+7];
+    dec(dc);
+  }
+  free(pixels);
   return 0;
 }
-
--- A06/encode.c
+++ A06/encode.c
@@ -3,6 +3,30 @@
 #include <stdlib.h>
 #include "read_ppm.h"
 #include "write_ppm.h"
+int* bin(char* arr){
+  int* binary = (int*)malloc(8*sizeof(arr));
+  int num;
+  for (int i = 0; i < 8*(sizeof(arr)); i+=8){
+    num = arr[i/8];
+    binary[i+7] = num%2;
+    num = num/2;
+    binary[i+6] = num%2;
+    num = num/2;
+    binary[i+5] = num%2;
+    num = num/2;
+    binary[i+4] = num%2;
+    num = num/2;
+    binary[i+3] = num%2;
+    num = num/2;
+    binary[i+2] = num%2;
+    num = num/2;
+    binary[i+1] = num%2;
+    num = num/2;
+    binary[i] = num%2;
+  }
+return binary;
+}
+
 
 int main(int argc, char** argv) {
   if (argc != 2) {
@@ -10,6 +34,34 @@ int main(int argc, char** argv) {
     return 0;
   }
 
+  int w, h;
+  int idx = 0;
+  char m[100];
+   
+  struct ppm_pixel** pixels = read_ppm(argv[1],&w,&h);
+  printf("m̴̛̜͎̭̰̮̾̐͊͐́͘͘̚͝͠m̶̞̟͓̭̂̊̃̓̾̈́̕͠m̸̨͇͇̺͍̳͈̜̱̣̘̈̉͗̒͆̀̈̿̌̕M̸̡̱̩̞̲̈́͆̃̈̇͂̓͒̄̚͝MESSAGE: ");
+  scanf("%s", m);
+  int *num = bin(m);
+  int x = 0;
+   for(int i = 0; i < h; i++){
+    for(int j = 0; j < w; j++){
+      pixels[i][j].red = (pixels[i][j].red & 0b11111110) | num[x] ;
+      x = x +1;
+      pixels[i][j].green = (pixels[i][j].green & 0b11111110) | num[x];
+      x = x +1;
+      pixels[i][j].blue = (pixels[i][j].blue & 0b11111110) | num[x];
+      x = x +1;
+    }
+  }
+  
+  char* nf = malloc(strlen(argv[1])+11);
+  strcpy(nf,argv[1]);
+  nf[strlen(nf)-4] = '\0';
+  strcat(nf,"-new.ppm");
+
+  write_ppm(nf, pixels, w, h);
+
+  free(pixels);
+  free(nf);
   return 0;
 }
-
