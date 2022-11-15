1ee56c6b7f77a1539b794926441b56aa08c4cb47
Joel Torres <jtorres@goldengate.cs.brynmawr.edu>
2022-11-15 15:23:43 -0500

    idk

--- A06/read_ppm.c
+++ A06/read_ppm.c
@@ -3,12 +3,35 @@
 #include <string.h>
 #include "read_ppm.h"
 
-struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
+// Choose *one* to implement
 
-  return NULL;
-}
+struct ppm_pixel** read_ppm_2d(const char* file, int* w, int* h) {
+  char arr[1000];//remember to change
+  FILE *fp;
+  fp=fopen(file,"rb");
+  fgets(arr,1000,fp);
+  if(arr[0]!='P'||arr[1]!='6'){
+    printf("You expect me to take a file like THAT?!?!");
+    return NULL;
+  }
+  //can use fread to copy data row by row into array
+  //print out thin
+  fgets(arr,1000, fp);
+  printf("\n%s",arr);
+  fgets(arr,1000, fp);
+  printf("\n%s",arr);
+  sscanf(arr,"%d %d",w,h);
+  printf("%d %d",*w,*h);
+  fgets(arr,1000, fp);
+  printf("\n%s",arr);
+  struct ppm_pixel** image =malloc(sizeof(struct ppm_pixel*)*(*h));
+  for(int i=0;i<*h;i++){
+    struct ppm_pixel*array=malloc(sizeof(struct ppm_pixel)*(*w));
+    image[i]=array;
+    fread(array,sizeof(struct ppm_pixel),*w,fp);
+  }
+  
 
-struct ppm_pixel** read_ppm_2d(const char* filename, int* w, int* h) {
-
-  return NULL;
+  fclose(fp);
+  return image;
 }
--- A06/write_ppm.c
+++ A06/write_ppm.c
@@ -2,8 +2,17 @@
 #include <stdio.h>
 #include <string.h>
 
-void write_ppm(const char* filename, struct ppm_pixel* pixels, int w, int h) {
+
+void write_ppm_2d(const char* filename, struct ppm_pixel** pixels, int w, int h) {
+    FILE* fp = fopen(filename, "wb");
+    fprintf(fp, "P6\n# Created by GIMP version 2.10.24 PNM plug-in\n%d %d\n225\n",w,h);
+    for (int i=0;i<h;i++) {
+        for (int j=0;j<w;j++) {
+            fwrite(&pixels[i][j].red,1,1,fp );
+            fwrite(&pixels[i][j].blue,1,1,fp );
+            fwrite(&pixels[i][j].green,1,1,fp );
+        }
+    }
+    fclose(fp);
 }
 
-void write_ppm_2d(const char* filename, struct ppm_pixel** pxs, int w, int h) {
-}
