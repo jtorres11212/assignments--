ea942a1a0327abf170b13aafd2fe6a029facbef0
Joel <jtorres@haverford.edu>
2022-12-06 15:48:27 -0500

    message

--- A09/multi_mandelbrot.c
+++ A09/multi_mandelbrot.c
@@ -10,6 +10,33 @@
 #include "read_ppm.h"
 #include "write_ppm.h"
 
+void compmbrot(struct ppm_pixel *pixels, int start_col , int end_col, int start_row, int end_row, struct ppm_pixel *palette, int size, int maxIterations, float xmin, float ymin, float xmax, float ymax){
+for(int i= start_row; i<end_row; i++){
+  for(int j=start_col; j<end_col; j++){
+    float xfrac = ((float) (j))/size;
+    float yfrac = ((float) (i))/size;
+    float x0 = xmin + xfrac * (xmax - xmin);
+    float y0 = ymin + yfrac * (ymax - ymin);
+    float x = 0.0;
+    float y = 0.0;
+    int iter = 0;
+    while (iter < maxIterations && x*x + y*y < 2*2){
+      float xtmp = x*x - y*y + x0;
+      y = 2*x*y + y0;
+      x = xtmp;
+      iter++;
+    }
+    if(iter < maxIterations){ //escaped
+      pixels[i][j] = palette[iter];
+    }
+    else{
+      pixels[i][j].red = 0;
+      pixels[i][j].green = 0;
+      pixels[i][j].blue = 0;
+    }
+  }
+}
+}
 int main(int argc, char* argv[]) {
   int size = 480;
   float xmin = -2.0;
@@ -17,8 +44,7 @@ int main(int argc, char* argv[]) {
   float ymin = -1.12;
   float ymax = 1.12;
   int maxIterations = 1000;
-  int numProcesses = 4;
-
+  int #process = 4;
   int opt;
   while ((opt = getopt(argc, argv, ":s:l:r:t:b:p:")) != -1) {
     switch (opt) {
@@ -28,15 +54,101 @@ int main(int argc, char* argv[]) {
       case 't': ymax = atof(optarg); break;
       case 'b': ymin = atof(optarg); break;
       case '?': printf("usage: %s -s <size> -l <xmin> -r <xmax> "
-        "-b <ymin> -t <ymax> -p <numProcesses>\n", argv[0]); break;
+        "-b <ymin> -t <ymax> -p <#process>\n", argv[0]); break;
     }
   }
-  printf("Generating mandelbrot with size %dx%d\n", size, size);
-  printf("  Num processes = %d\n", numProcesses);
-  printf("  X range = [%.4f,%.4f]\n", xmin, xmax);
-  printf("  Y range = [%.4f,%.4f]\n", ymin, ymax);
+  printf("Generating mandelbrot w/ size %dx%d\n", size, size);
+  printf("# processes = %d\n", #process);
+  printf("x range = [%.4f,%.4f]\n", xmin, xmax);
+  printf("y range = [%.4f,%.4f]\n", ymin, ymax);
 
-  // todo: your code here
-  // generate pallet
-  // compute image
+int SHMIDL;
+SHMIDL= shmget(IPC_PRIVATE, sizeof(struct ppm_pixel) * maxIterations, 0644 | IPC_CREAT);
+if (SHMIDL == -1) {
+  perror("Error: cannot initialize shared memory\n");
+  exit(1);
 }
+struct ppm_pixel *palette = shmat(SHMIDL, NULL, 0);
+for(int i=0; i<maxIterations; i++){
+  palette[i].red = rand() % 255;
+  palette[i].green = rand() %255;
+  palette[i].blue = rand()% 255;
+}
+int shmid;
+shmid= shmget(IPC_PRIVATE, sizeof(struct ppm_pixel) * size * size, 0644 | IPC_CREAT);
+if (shmid == -1) {
+  perror("Error: cannot initialize shared memory\n");
+  exit(1);
+}
+struct ppm_pixel *pixels = shmat(shmid, NULL, 0);
+if(pixels ==(void*)-1){
+  perror("Error: cannot access shared memory\n");
+    exit(1);
+}
+double timer;
+struct timeval tstart, tend; 
+gettimeofday(&tstart, NULL);
+for (int i= 0; i <4; i++){
+  fflush(stdout);
+  int pid =  fork();
+  if (pid == 0){
+    if ( i == 0){
+      printf("%d)Sub-image block: cols (0, 240) to rows (0,240)\n", getpid());
+      fflush(stdout);
+(s, 0, 240, 0, 240,  palette, size,maxIterations, xmin, ymin, xmax,  ymax);
+    }
+    else if(i == 1){
+      printf("%d)Sub-image block: cols (240, 480) to rows (0,240)\n", getpid());
+      fflush(stdout);
+(s, 240, 480, 0, 240,  palette, size,maxIterations, xmin, ymin, xmax,  ymax);
+    }
+    else if(i == 2){
+      printf("%d)Sub-image block: cols (0, 240) to rows (240,480)\n", getpid());
+      fflush(stdout);
+(s, 0, 240, 240, 480,  palette, size,maxIterations, xmin, ymin, xmax,  ymax);
+    }
+
+    else if(i == 3){
+      printf("%d)Sub-image block: cols (240, 480) to rows (240,480)\n", getpid()); 
+      fflush(stdout);
+(s, 240, 480, 240, 480,  palette, size,maxIterations, xmin, ymin, xmax,  ymax);
+    }
+     exit(0);
+  }
+  else{
+    printf("Launched child process: %d\n", pid);
+  }
+}
+  for (int i = 0; i < 4; i++) {
+    int status;
+    int pid = wait(&status);
+    printf("Child process complete: %d\n", pid);
+  }
+gettimeofday(&tend, NULL);
+
+timer = tend.tv_sec - tstart.tv_sec + (tend.tv_usec-tstart.tv_usec)/1.e6;
+int timestamp = time(0);
+printf("Computed mandelbrot set (%dx%d) in %f seconds\n", size, size,timer);
+  char fp[100];
+  sprintf(fp, "multi-mandelbrot-%d-%d.ppm", size, timestamp);
+  printf("Writing file: %s\n", fp);
+  write_ppm(fp, pixels, size, size);
+  if (shmdt(pixels) == -1) {
+    perror("Error: cannot detatch from shared memory\n");
+    exit(1);
+  }
+  if (shmctl(shmid, IPC_RMID, 0) == -1) {
+    perror("Error: cannot remove shared memory\n");
+    exit(1);
+  } 
+
+  if (shmdt(palette) == -1) {
+    perror("Error: cannot detatch from shared memory\n");
+    exit(1);
+  }
+
+  if (shmctl(SHMIDL, IPC_RMID, 0) == -1) {
+    perror("Error: cannot remove shared memory\n");
+    exit(1);
+  } 
+}  
--- A09/single_mandelbrot.c
+++ A09/single_mandelbrot.c
@@ -13,7 +13,6 @@ int main(int argc, char* argv[]) {
   float ymin = -1.12;
   float ymax = 1.12;
   int maxIterations = 1000;
-
   int opt;
   while ((opt = getopt(argc, argv, ":s:l:r:t:b:")) != -1) {
     switch (opt) {
@@ -28,11 +27,53 @@ int main(int argc, char* argv[]) {
   printf("Generating mandelbrot with size %dx%d\n", size, size);
   printf("  X range = [%.4f,%.4f]\n", xmin, xmax);
   printf("  Y range = [%.4f,%.4f]\n", ymin, ymax);
-
-  // todo: your work here
-  // generate pallet
   srand(time(0));
+struct ppm_pixel *palette = malloc(sizeof(struct ppm_pixel)* maxIterations);
+for(int i=0; i<maxIterations; i++){
+  palette[i].red = rand() % 255;
+  palette[i].green = rand() %255;
+  palette[i].blue = rand()% 255;
+}
 
-  // compute image
+struct ppm_pixel * pixels = malloc(sizeof(struct ppm_pixel)*size *size);
 
+double timer;
+struct timeval tstart, tend; 
+  gettimeofday(&tstart, NULL);
+for(int i= 0; i<size; i++){
+  for(int j=0; j<size; j++){
+    float xfrac = (float) (j/size);
+    float yfrac = (float) (i/size);
+    float x0 = xmin + xfrac * (xmax - xmin);
+    float y0 = ymin + yfrac * (ymax - ymin);
+  
+    float x = 0.0;
+    float y = 0.0;
+    int iter = 0;
+    while (iter < maxIterations && x*x + y*y < 2*2){
+      float xtmp = x*x - y*y + x0;
+      y = 2*x*y + y0;
+      x = xtmp;
+      iter++;
+    }
+    if(iter < maxIterations){
+      pixels[i][j] = palette[iter];
+    }
+    else{
+      pixels[i][j].red = 0;
+      pixels[i][j].green = 0;
+      pixels[i][j].blue = 0;
+    }
+  }
+}
+gettimeofday(&tend, NULL);
+timer = tend.tv_sec - tstart.tv_sec + (tend.tv_usec-tstart.tv_usec)/1.e6;
+int timestamp = time(0);
+printf("Computed mandelbrot set (%dx%d) in %f seconds\n", size, size,timer);
+  char fp[100];
+  sprintf(fp, "mandelbrot-%d-%d.ppm", size, timestamp);
+  printf("Writing file: %s\n", fp);
+  write_ppm(fp, pixels, size, size);
+  free(pixels);
+  free(palette);
 }
