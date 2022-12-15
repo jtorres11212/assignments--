55fc6b25d9815421a437a0e328f42a49356c963f
Joel <jtorres@haverford.edu>
2022-12-15 09:31:48 -0500

    it was a pleasure

--- A10/buddhabrot.c
+++ A10/buddhabrot.c
@@ -32,7 +32,120 @@ int main(int argc, char* argv[]) {
   printf("  Num processes = %d\n", numProcesses);
   printf("  X range = [%.4f,%.4f]\n", xmin, xmax);
   printf("  Y range = [%.4f,%.4f]\n", ymin, ymax);
-
+  srand(time(0));
+  struct ppm_pixel *pal=malloc(sizeof(struct ppm_pixel)*maxIterations);
+  for(int i=0;i<maxIterations; i++){
+    pal[i].red=rand() %255;
+    pal[i].green=rand() %255;
+    pal[i].blue=rand() %255;
+  }
+  struct ppm_pixel *pix=malloc(sizeof(struct ppm_pixel)*size*size);
+  int *mem,*count=malloc(sizeof(int)*size*size);
+  for(int i=0;i<size;i++;){
+    for(int i=0;i<size;i++;){
+      count[i][j]=0
+    }
+  }
+  int con=0;
+  double t;
+  struct timeval tstart,tend;
+  gettimepfday(&tstart,NULL);
+  pthread_t threads[4];
+  pthread_mutex_init(&mutex, NULL);
+  pthread_barrier_init(&barrier,NULL,4);
+  struct thread_data data[4];
+  for(int i=0;i<4;i++){
+    if(i=0){
+      printf("%d. Sub image block: collumns 0,240 to rows 0,240")
+      data[i].id=1;
+      data[i].pix=pix;
+      data[i].start_col=0;
+      data[i].end_col=240;
+      data[i].start_row=0;
+      data[i].end_row=240;
+      data[i].pal=pal;
+      data[i].maxIterations=maxIterations;
+      data[i].xmin=xmin;
+      data[i].ymin=ymin;
+      data[i].xmax=xmax;
+      data[i].ymax=ymax;
+      data[i].mem=mem;
+      data[i].count=count;
+      data[i].con=&con;
+      pthread_create(&threads[i],NULL,do_formula,(void*)&data[i])
+    }
+    if(i=1){
+      printf("%d. Sub image block: collumns 240,480 to rows 0,240")
+      data[i].id=i;
+      data[i].pix=pix;
+      data[i].start_col=0;
+      data[i].end_col=240;
+      data[i].start_row=0;
+      data[i].end_row=240;
+      data[i].pal=pal;
+      data[i].maxIterations=maxIterations;
+      data[i].xmin=xmin;
+      data[i].ymin=ymin;
+      data[i].xmax=xmax;
+      data[i].ymax=ymax;
+      data[i].mem=mem;
+      data[i].count=count;
+      data[i].con=&con;
+      pthread_create(&threads[i],NULL,do_formula,(void*)&data[i])
+    }
+if(i=2){
+      printf("%d. Sub image block: collumns 0,240 to rows 0,240")
+      data[i].id=i;
+      data[i].pix=pix;
+      data[i].start_col=0;
+      data[i].end_col=240;
+      data[i].start_row=240;
+      data[i].end_row=480;
+      data[i].pal=pal;
+      data[i].maxIterations=maxIterations;
+      data[i].xmin=xmin;
+      data[i].ymin=ymin;
+      data[i].xmax=xmax;
+      data[i].ymax=ymax;
+      data[i].mem=mem;
+      data[i].count=count;
+      data[i].con=&con;
+      pthread_create(&threads[i],NULL,do_formula,(void*)&data[i])
+    }
+if(i=3){
+      printf("%d. Sub image block: collumns 0,240 to rows 0,240")
+      data[i].id=i;
+      data[i].pix=pix;
+      data[i].start_col=240;
+      data[i].end_col=480;
+      data[i].start_row=240;
+      data[i].end_row=480;
+      data[i].pal=pal;
+      data[i].maxIterations=maxIterations;
+      data[i].xmin=xmin;
+      data[i].ymin=ymin;
+      data[i].xmax=xmax;
+      data[i].ymax=ymax;
+      data[i].mem=mem;
+      data[i].count=count;
+      data[i].con=&con;
+      pthread_create(&threads[i],NULL,do_formula,(void*)&data[i])
+    }
+  }
+  for(int i=0;i<4;){
+    pthread_join(threads[i],NULL);
+  }
+  gettimeofday(&tend,NULL);
+  t=tend.tv_sec-tstart.tv_sec+(tend.tv_usec-tstart.tv_usec)/1.e6;
+  int timestamp=time(0)
+  printf("Buddahbrot set %dx%d computed!It took %f seconds\n",size,size,t)
+  write_ppm(fp,pix,size,size);
+  pthread_mutex_destroy(&mutex);
+  pthread_barrier_destroy(&barrier);
+  free(pix);
+  free(pal);
+  free(mem);
+  free(count);
   // todo: your code here
   // compute image
 }
--- A10/thread_mandelbrot.c
+++ A10/thread_mandelbrot.c
@@ -1,11 +1,32 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <assert.h>
-#include <time.h>
-#include <pthread.h>
-#include "read_ppm.h"
-#include "write_ppm.h"
+void compute_mandelbrot(struct ppm_pixel *pixels, int start_col , int end_col, int start_row, int end_row, struct ppm_pixel *palette, int size, int maxi, float xmin, float ymin, float xmax, float ymax){
+for(int i= start_row; i<end_row; i++){
+  for(int j=start_col; j<end_col; j++){
+    float xfrac = ((float) (j))/size;
+    float yfrac = ((float) (i))/size;
+    float x0 = xmin + xfrac * (xmax - xmin);
+    float y0 = ymin + yfrac * (ymax - ymin);
+  
+    float x = 0.0;
+    float y = 0.0;
+    int iter = 0;
+    while (iter < maxi && x*x + y*y < 2*2){
+      float xtmp = x*x - y*y + x0;
+      y = 2*x*y + y0;
+      x = xtmp;
+      iter++;
+    }
+    if(iter < maxi){ //escaped
+      pixels[i * size + j] = palette[iter];
+    }
+    else{
+      pixels[i * size + j].red = 0;
+      pixels[i * size + j].green = 0;
+      pixels[i * size + j].blue = 0;
+    }
+  }
+}
+}
+
 
 int main(int argc, char* argv[]) {
   int size = 480;
@@ -13,7 +34,7 @@ int main(int argc, char* argv[]) {
   float xmax = 0.47;
   float ymin = -1.12;
   float ymax = 1.12;
-  int maxIterations = 1000;
+  int maxi = 1000;
   int numProcesses = 4;
 
   int opt;
@@ -33,7 +54,99 @@ int main(int argc, char* argv[]) {
   printf("  X range = [%.4f,%.4f]\n", xmin, xmax);
   printf("  Y range = [%.4f,%.4f]\n", ymin, ymax);
 
-  // todo: your code here
-  // generate pallet
-  // compute image
+int shmid1;
+shmid1= shmget(IPC_PRIVATE, sizeof(struct ppm_pixel) * maxi, 0644 | IPC_CREAT);
+if (shmid1 == -1) {
+  perror("Error: cannot initialize shared memory\n");
+  exit(1);
 }
+
+struct ppm_pixel *palette = shmat(shmid1, NULL, 0);
+for(int i=0; i<maxi; i++){
+  palette[i].red = rand() % 255;
+  palette[i].green = rand() %255;
+  palette[i].blue = rand()% 255;
+}
+int shmid;
+shmid= shmget(IPC_PRIVATE, sizeof(struct ppm_pixel) * size * size, 0644 | IPC_CREAT);
+if (shmid == -1) {
+  perror("Error: cannot initialize shared memory\n");
+  exit(1);
+}
+struct ppm_pixel *pixels = shmat(shmid, NULL, 0);
+if(pixels ==(void*)-1){
+  perror("Error: cannot access shared memory\n");
+    exit(1);
+}
+double timer;
+struct timeval tstart, tend; 
+gettimeofday(&tstart, NULL);
+for (int i= 0; i <4; i++){
+  fflush(stdout);
+  int pid =  fork();
+  if (pid == 0){
+    if ( i == 0){
+      printf("%d) bok: cols (0, 240) to rows (0,240)\n", getpid());
+      fflush(stdout);
+      compute_mandelbrot(pixels, 0, 240, 0, 240,  palette, size,maxi, xmin, ymin, xmax,  ymax);
+    }
+
+    else if(i == 1){
+      printf("%d)Subimg block: cols (240, 480) to rows (0,240)\n", getpid());
+      fflush(stdout);
+      compute_mandelbrot(pixels, 240, 480, 0, 240,  palette, size,maxi, xmin, ymin, xmax,  ymax);
+    }
+
+    else if(i == 2){
+      printf("%d) bok: cols (0, 240) to rows (240,480)\n", getpid());
+      fflush(stdout);
+      compute_mandelbrot(pixels, 0, 240, 240, 480,  palette, size,maxi, xmin, ymin, xmax,  ymax);
+    }
+
+    else if(i == 3){
+      printf("%d) bok: cols (240, 480) to rows (240,480)\n", getpid()); 
+      fflush(stdout);
+      compute_mandelbrot(pixels, 240, 480, 240, 480,  palette, size,maxi, xmin, ymin, xmax,  ymax);
+    }
+     exit(0);
+  }
+  else{
+    printf("Launched child process: %d\n", pid);
+  }
+}
+  for (int i = 0; i < 4; i++) {
+    int status;
+    int pid = wait(&status);
+    printf("Child process complete: %d\n", pid);
+  }
+gettimeofday(&tend, NULL);
+timer = tend.tv_sec - tstart.tv_sec + (tend.tv_usec-tstart.tv_usec)/1.e6;
+int time= time(0);
+printf("Computed mandelbrot set (%dx%d) in %f seconds\n", size, size,timer);
+
+  char fp[100];
+  sprintf(fp, "multi-mandelbrot-%d-%d.ppm", size, time
+;
+  printf("Writing file: %s\n", fp);
+  write_ppm(fp, pixels, size, size);
+
+
+  if (shmdt(pixels) == -1) {
+    perror("Error: cannot detatch from shared memory\n");
+    exit(1);
+  }
+  if (shmctl(shmid, IPC_RMID, 0) == -1) {
+    perror("Error: cannot remove shared memory\n");
+    exit(1);
+  } 
+
+  if (shmdt(palette) == -1) {
+    perror("Error: cannot detatch from shared memory\n");
+    exit(1);
+  }
+
+  if (shmctl(shmid1, IPC_RMID, 0) == -1) {
+    perror("Error: cannot remove shared memory\n");
+    exit(1);
+  } 
+}  
